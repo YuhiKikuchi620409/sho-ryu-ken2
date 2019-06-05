d178d037daf25143b87eb7316696fa1c99873fec
Yuhi-Kikuchi620409 <snow.1ove-1ife-_-0409@ezweb.ne.jp>
2019-06-05 13:00:47 +0900

    Initial Commit

---
+++ sho-ryu-ken2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sho-ryu-ken2
+//
+//  Created by 菊地雄飛 on 2019/06/05.
+//  Copyright © 2019 菊地雄飛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
