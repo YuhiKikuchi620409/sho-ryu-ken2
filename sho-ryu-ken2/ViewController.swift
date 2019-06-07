9c477d2f70a6f10c3d4114effc786c9a5f38da54
Yuhi-Kikuchi620409 <snow.1ove-1ife-_-0409@ezweb.ne.jp>
2019-06-07 17:51:53 +0900

    昇竜拳できました

--- sho-ryu-ken2/ViewController.swift
+++ sho-ryu-ken2/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // 昇竜拳画像の格納配列
+       var imageArray : Array<UIImage> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        shoryuken.image = UIImage(named:"attak1") //初期画面用
+
+        while let attackImage = UIImage(named: "attak\(imageArray.count+1)") {
+            imageArray.append(attackImage)
+        }
     }
-
-
+    
+ 
+    @IBOutlet weak var shoryuken: UIImageView!
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        // アニメーションの適用
+        shoryuken.animationImages = imageArray
+        // アニメーションの長さ:2秒
+        shoryuken.animationDuration = 2
+        // アニメーション再生回数:1回
+        shoryuken.animationRepeatCount = 1
+        // アニメーションを開始
+        shoryuken.startAnimating()
+        
+        //2秒後に実行したい処理  勝利宣言  遊びです
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            
+//        }
+        //1秒後にタイトルに戻る
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+        }
+        
 }
 
+}
